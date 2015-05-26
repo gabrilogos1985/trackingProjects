19887bca3ae5f04ff62560d8fbd02b5a8d7168c9
Gabe <gabriel.paz@correo.uaq.mx>
2015-05-25 23:56:57 -0500

    Initial commit

---
+++ src/main/java/gps/tracking/TrackingProyectsApplication.java
@@ -0,0 +1,12 @@
+package gps.tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingProyectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackingProyectsApplication.class, args);
+    }
+}
